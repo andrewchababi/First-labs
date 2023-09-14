9d89cdf8f1c418a097f4ce9121cfc44f1d536e6d
Andrew Chababi <andrewchabab6109@gmail.com>
2023-09-14 15:24:54 -0400

    work on this one

--- INF1007/INF1007_practice/Chap_4_ex.py
+++ INF1007/INF1007_practice/Chap_4_ex.py
@@ -1,1 +1,23 @@
 # on parle de list et chain de charctere
+#on va commence les problemes 
+
+count = 0
+chain = ''
+
+for character in chain:
+    if character >= 'a' and character <= 'z':
+        count += 1 
+
+if count == len(chain) and len(chain)>0:
+    print(True)
+else:
+    print(False)
+    
+#option 2
+
+result = True
+for charac in chain:
+    if 'A' <= charac <= 'Z':
+        result = False
+        break
+        
