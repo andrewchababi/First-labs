343041229cd0c31ef3ec6b9b8407ef66f0263629
Andrew <andrewchabab6109@gmail.com>
2023-09-18 23:51:21 -0400

    fixed

--- INF1007/INF1007_practice/Chap_5_ex.py
+++ INF1007/INF1007_practice/Chap_5_ex.py
@@ -1,15 +1,11 @@
 from random import randint
-computer_choice = 8
+computer_choice = randint(1,10)
 
-user_choice = int(input("Hey try to guess my number !! " ))
-
-bool1 = user_choice == computer_choice
+user_choice = int(input("Hey try to guess my number !! "))
 
 while user_choice != computer_choice:
-    user_choice = input("Hey try to guess my number !! " )
     print(user_choice)
     print(computer_choice)
-    if user_choice == computer_choice:
-        break
-    
-print("you win")
+    user_choice = int(input("Try again: "))
+
+print("You win!")
