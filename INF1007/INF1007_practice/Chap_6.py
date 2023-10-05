f44709304f92e50d80a467d1e57d6fcb3c9b86a2
Andrew <andrewchabab6109@gmail.com>
2023-10-04 22:17:15 -0400

    solve chap 6

--- INF1007/INF1007_practice/Chap_6.py
+++ INF1007/INF1007_practice/Chap_6.py
@@ -26,3 +26,52 @@ if len(list_1)== len(list_2):
             break
 else:
     resulta = False
+
+
+ma_list = list(range(10))
+#print(ma_list)
+
+nouvelle_list = []
+for element in ma_list:
+    nouvelle_list.append(element*2)
+#print(nouvelle_list)
+
+#instead of all that what we could do is:
+
+nouvelle_list = [element*2 for element in ma_list]
+#print(nouvelle_list)
+
+mon_dictionaire = {"a":1, "b":2, "c":3}
+
+list_a = []
+for key in mon_dictionaire:
+    list_a.append((key, mon_dictionaire[key]))
+#print(list_a)
+
+list_a = [(key, mon_dictionaire[key]) for key in mon_dictionaire] 
+#print(list_a)
+
+#this doesn't really work you almost got it but when not using a dictionary yoou can only be so efficient
+#in terms of addressing a large amount of data at once because you have to start accounting for every 
+#case yourself
+
+open_bracket_stack = []
+text = "[({)]}["
+
+open_bracket_stack = []
+for character in text:
+    if character in ["(","{","["]:
+        open_bracket_stack.append(character)
+        print(open_bracket_stack)
+
+    elif character in [")","]","}"]:
+        if len(open_bracket_stack) != 0:
+            res = open_bracket_stack.pop()
+            
+            print(open_bracket_stack)
+        else:
+            print("false")
+            break
+
+    else: 
+        print("all good")
